3610282538fbb35115b48f5bf6cda299d46590ca
Aiden20021 <134684046+Aiden20021@users.noreply.github.com>
2023-11-29 11:43:12 +0100

    Update holiday.js

--- js/holiday.js
+++ js/holiday.js
@@ -1,61 +1,44 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-    console.log('holiday.js - in content loaded event');
+  console.log('holiday.js - in content loaded event');
 
-    // fetchNearestStations(51.19417, 5.9875);
-    // fetchNSDisruptions();
-    // fetchPublicHolidays(countryCode, year); 
-  
-    // const countryCode = '+31'; 
-    // const year = 2024;
+  const refreshInterval = 2 * 60 * 1000;
 
-    const refreshInterval = 2 * 60 * 1000;
-  
-    getPublicHolidays();
+  getPublicHolidays();
 
-    setInterval(function () {
-      // fetchNearestStations(51.19417, 5.9875);
-      // fetchNSDisruptions();
-      // fetchPublicHolidays(countryCode, year);
-      getPublicHolidays()
-    }, refreshInterval);
-  });
+  setInterval(function () {
+    getPublicHolidays()
+  }, refreshInterval);
+});
 
-  
 function getPublicHolidays() {
 
-  console.log('getting public holidays...')
+console.log('getting public holidays...')
 
-  const countryCode = encodeURIComponent("+31");
-  const year = 2024;
+const countryCode = encodeURIComponent("+31");
+const year = 2024;
 
-  const apiUrl = `https://date.nager.at/api/v3/NextPublicHolidays/${countryCode}`;
+const apiUrl = `https://date.nager.at/api/v3/NextPublicHolidays/${countryCode}`;
 
-  fetch(apiUrl)
-      .then(response => response.json())
-      .then(data => displayHolidays(data))
-      .catch(error => console.error('Error fetching data:', error));
+fetch(apiUrl)
+    .then(response => response.json())
+    .then(data => displayHolidays(data))
+    .catch(error => console.error('Error fetching data:', error));
 }
 
 function displayHolidays(holidays) {
 
-  console.log('displaying public holidays...')
+console.log('displaying public holidays...')
 
-  // const holidaysListEl(ement)
-  const holidaysList = document.getElementById('holidaysList');
+const holidaysList = document.getElementById('holidaysList');
 
-  if (holidays && holidays.length > 0) {
-    const listItems = holidays.map(holiday => {
-      return `<p style="font-size: 14px;">${holiday.name}, <br> ${holiday.date}</p>`;
-      // Adjust font-size and color values according to your preferences
-    });
+if (holidays && holidays.length > 0) {
+  const listItems = holidays.map(holiday => {
+    return `<p style="font-size: 14px;">${holiday.name}, <br> ${holiday.date}</p>`;
+  });
 
-    holidaysList.innerHTML = `<ul>${listItems.join('')}</ul>`;
-  } else {
-    holidaysList.innerHTML = '<p style="font-size: 16px; color: #333;">No holidays data available.</p>';
-    // Adjust font-size and color values according to your preferences
-  }
+  holidaysList.innerHTML = `<ul>${listItems.join('')}</ul>`;
+} else {
+  holidaysList.innerHTML = '<p style="font-size: 16px; color: #333;">No holidays data available.</p>';
 }
-
-// Call the function to fetch and display holidays on page load
-// getPublicHolidays();
+}

85be450b6912f47ca47277a814003eb2a34742a3
Sten Overdijk <stenoverdijk@gmail.com>
2023-11-30 14:18:51 +0100

    NS Departures Update
    You can now see Departure time and delays, Destination, in-between stops and platform where it leaves from.

--- ns.js
+++ ns.js
@@ -34,14 +34,16 @@ function getDepartures() {
     });
 }
 
+// ns.js
+
 function displayDepartures(data) {
-    const departuresContainer = document.getElementById('departures');
-    departuresContainer.innerHTML = ''; // Clear previous content
+    const departuresTableBody = document.getElementById('departuresTableBody');
+    departuresTableBody.innerHTML = ''; // Clear previous content
 
     const payload = data && data.payload;
 
     if (payload && payload.departures && payload.departures.length > 0) {
-        // Create and append elements for each departure
+        // Create and append rows for each departure
         payload.departures.forEach(departure => {
             const plannedDepartureTime = new Date(departure.plannedDateTime);
             const actualDepartureTime = departure.actualDateTime ? new Date(departure.actualDateTime) : null;
@@ -56,42 +58,71 @@ function displayDepartures(data) {
             if (actualDepartureTime) {
                 const delayMinutes = Math.round((actualDepartureTime - plannedDepartureTime) / (1000 * 60));
                 if (delayMinutes > 0) {
-                    delayInfo = ` (Vertraging: ${delayMinutes} minuten)`;
+                    // Create a red box for the delay information
+                    delayInfo = `<span class="bg-danger text-white p-1 rounded">+${delayMinutes} min</span>`;
                 }
             }
 
-            // Create a div for each departure
-            const departureDiv = document.createElement('div');
+            // Create a table row for each departure
+            const departureRow = document.createElement('tr');
 
-            // Create a span for the main destination with bold and larger font
-            const mainDestination = document.createElement('span');
-            mainDestination.style.fontWeight = 'bold';
-            mainDestination.style.fontSize = '1.2em'; // Adjust the size as needed
-            mainDestination.textContent = `Trein naar ${departure.direction} om ${formattedPlannedDepartureTime}${delayInfo}`;
+            // Create and append cells for each departure detail
+            const timeCell = document.createElement('td');
+            timeCell.innerHTML = `${formattedPlannedDepartureTime} ${delayInfo}`;
 
-            // Include in-between stops in a separate paragraph
-            const inBetweenStops = departure.routeStations.map(stop => stop.mediumName).join(', ');
-            const inBetweenStopsInfo = document.createElement('p');
-            inBetweenStopsInfo.textContent = `Tussenstops: ${inBetweenStops}`;
+            const directionCell = document.createElement('td');
+            directionCell.textContent = departure.direction;
 
-            // Append the elements to the departure div
-            departureDiv.appendChild(mainDestination);
-            departureDiv.appendChild(inBetweenStopsInfo);
+            // If there are in-between stops, create a cell for them
+            const inBetweenStopsCell = document.createElement('td');
+            let inBetweenStopsText = '';
 
-            // Append the departure div to the departures container
-            departuresContainer.appendChild(departureDiv);
+            if (departure.routeStations && departure.routeStations.length > 0) {
+                // Use routeStations if available
+                inBetweenStopsText = departure.routeStations
+                    .map(stop => stop.mediumName)
+                    .join(', '); // Comma-separated format
+
+                // Include the main destination (first stop) if there's only one stop
+                if (departure.routeStations.length === 1) {
+                    inBetweenStopsText = departure.routeStations[0].mediumName;
+                }
+            }
+
+            // Set the text content to the in-between stops or an empty string
+            inBetweenStopsCell.textContent = inBetweenStopsText;
+
+            const trackCell = document.createElement('td');
+            trackCell.textContent = departure.plannedTrack;
+
+            // Append cells to the row
+            departureRow.appendChild(timeCell);
+            departureRow.appendChild(directionCell);
+
+            // Append the in-between stops cell only if there are in-between stops
+            if (inBetweenStopsText !== '') {
+                departureRow.appendChild(inBetweenStopsCell);
+            } else {
+                // If no in-between stops, create an empty cell
+                const emptyCell = document.createElement('td');
+                departureRow.appendChild(emptyCell);
+            }
+
+            departureRow.appendChild(trackCell);
+
+            // Append the row to the departures table body
+            departuresTableBody.appendChild(departureRow);
         });
     } else {
         // If no departures are available, display a message
-        const noDeparturesMessage = document.createElement('p');
-        noDeparturesMessage.textContent = 'Geen vertrekken beschikbaar.';
-        departuresContainer.appendChild(noDeparturesMessage);
+        const noDeparturesRow = document.createElement('tr');
+        const noDeparturesCell = document.createElement('td');
+        noDeparturesCell.colSpan = 4; // Span 4 columns
+        noDeparturesCell.className = 'text-center';
+        noDeparturesCell.textContent = 'Geen vertrekken beschikbaar.';
+        noDeparturesRow.appendChild(noDeparturesCell);
+        departuresTableBody.appendChild(noDeparturesRow);
     }
 }
-
-
-
-
-
 
 

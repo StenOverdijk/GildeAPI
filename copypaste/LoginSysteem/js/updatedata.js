fb18b960889c6425f8394d236e416a810c15501a
samd909 <112548067+samd909@users.noreply.github.com>
2023-12-11 14:46:23 +0100

    updated
    updated

---
+++ copypaste/LoginSysteem/js/updatedata.js
@@ -0,0 +1,61 @@
+document.addEventListener('DOMContentLoaded', function() {
+    getAllData();
+  });
+  
+  function getAllData() {
+    fetch('http://localhost:3000/getAllData')
+      .then(response => response.json())
+      .then(data => {
+        const allDataContainer = document.getElementById('allData');
+        if (data && data.length > 0) {
+          const rowsHTML = data.map(row => {
+            return `
+            <div>            
+            <p><strong>Datum:</strong> ${row.datum}</p>
+            <p><strong>Titel:</strong> ${row.titel}</p>
+            <p><strong>Inhoud:</strong> ${row.inhoud}</p>
+            </div>`;
+          }).join('');
+  
+          allDataContainer.innerHTML = `<div>${rowsHTML}</div>`;
+        } else {
+          allDataContainer.innerHTML = 'No data available.';
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+
+
+  
+  function updateData() {
+    const datumData = document.getElementById('datum').value;
+    const titelData = document.getElementById('titel').value;
+    const inhoudData = document.getElementById('inhoud').value;
+
+    if (!datumData || !titelData || !inhoudData) {
+      console.log('All fields are required');
+      return;
+    }
+
+    fetch('http://localhost:3000/updateData', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        datumData,
+        titelData,
+        inhoudData,
+      }),
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      getLatestData();
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  }
--- js/main.js
+++ js/main.js
@@ -1,36 +1,48 @@
 c1Btn = document.getElementById("c1Btn")
 c2Btn = document.getElementById("c2Btn")
 c3Btn = document.getElementById("c3Btn")
+c4Btn = document.getElementById("c4Btn")
 container1 = document.getElementById("container1")
 container2 = document.getElementById("container2")
 container3 = document.getElementById("container3")
+container4 = document.getElementById("container4")
 tableNS = document.getElementById("tableNS")
 
 
 container1.style.display = 'block'
 container2.style.display = 'none'
 container3.style.display = 'none'
+container3.style.display = 'none'
 tableNS.style.display = 'none'
 
 c1Btn.addEventListener("click", ()=> {
-    tableNS.style.display = 'block'
     container1.style.display = 'block'
     container2.style.display = 'none'
     container3.style.display = 'none'
+    container4.style.display = 'none'
+    tableNS.style.display = 'block'
 })
 
 c2Btn.addEventListener("click", ()=> {
     container1.style.display = 'none'
     container2.style.display = 'block'
     container3.style.display = 'none'
+    container4.style.display = 'none'
     tableNS.style.display = 'none'
-
 })
 
 c3Btn.addEventListener("click", ()=> {
     container1.style.display = 'none'
     container2.style.display = 'none'
     container3.style.display = 'block'
+    container4.style.display = 'none'
     tableNS.style.display = 'none'
+})
 
+c3Btn.addEventListener("click", ()=> {
+    container1.style.display = 'none'
+    container2.style.display = 'none'
+    container3.style.display = 'none'
+    container4.style.display = 'block'
+    tableNS.style.display = 'none'
 })

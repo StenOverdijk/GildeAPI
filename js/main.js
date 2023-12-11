e903cc83d26f6217e16bc259d3712483fb22fbac
samd909 <112548067+samd909@users.noreply.github.com>
2023-12-11 13:18:39 +0100

    Main Design
    Fortnite

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+
+
+container1Btn.addEventListener("click", () => {
+    container1Btn = document.getElementById("container1Btn")
+    document.getElementById("container1").style.display = "none"
+})
+
+container2Btn.addEventListener("click", () => {
+    container2Btn = document.getElementById("container2Btn")
+    document.getElementById("container2").style.display = "none"
+})
+
+container3Btn.addEventListener("click", () => {
+    container3Btn = document.getElementById("container3Btn")
+    document.getElementById("container3").style.display = "none"
+})
